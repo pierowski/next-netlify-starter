a66d388dfb2a2941f33476c51d537278e72208be
pierowski <pierpaoloroberto@gmail.com>
2024-07-02 11:42:59 +0200

    :construction:

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,27 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { useEffect } from 'react';
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const { code } = router.query;
+
+    if (code) {
+      const baseUrl = 'https://d26paarabrky6y.cloudfront.net/';
+      const fullUrl = `${baseUrl}?code=${code}`;
+      window.location.href = fullUrl;
+    }
+  }, [router.query]);
+
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
-        <script type="text/javascript">
-          window.location.href = "https://d26paarabrky6y.cloudfront.net/";
-        </script>
       </Head>
 
       <main>
