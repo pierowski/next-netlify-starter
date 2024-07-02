23f1aa5cadc6c0055464e0bc376d2752a8f099df
pierowski <pierpaoloroberto@gmail.com>
2024-07-02 13:59:18 +0200

    :construction:

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
   const router = useRouter();
 
   const getFullUrl = (code) => {
+    debugger;
     const baseUrl = 'https://d26paarabrky6y.cloudfront.net/';
     return `${baseUrl}${JSON.stringify(code)}`;
   };
