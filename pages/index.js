a5517bad62367ca857b3d4818d996074268a82a1
pierowski <pierpaoloroberto@gmail.com>
2024-07-02 11:58:04 +0200

    :construction:

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,17 @@ export default function Home() {
     const { code } = router.query;
 
     if (code) {
-      const baseUrl = 'https://d26paarabrky6y.cloudfront.net/';
-      const fullUrl = `${baseUrl}?code=${code}`;
-      window.location.href = fullUrl;
+      const linkElement = document.getElementById('redirect-link');
+      if (linkElement) {
+        linkElement.click();
+      }
     }
   }, [router.query]);
+
+  const getFullUrl = (code) => {
+    const baseUrl = 'https://d26paarabrky6y.cloudfront.net/';
+    return `${baseUrl}?code=${code}`;
+  };
 
   return (
     <div className="container">
@@ -27,7 +33,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Se non vieni reindirizzato automaticamente, clicca <a href="https://d26paarabrky6y.cloudfront.net/">qui</a>.
+          Se non vieni reindirizzato automaticamente, clicca <a id="redirect-link" href={getFullUrl(router.query.code)}>qui</a>.
         </p>
       </main>
 
