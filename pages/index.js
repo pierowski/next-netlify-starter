436ae3fbd4d02a3a3ba2376c92a68c0740d6bb2b
pierowski <pierpaoloroberto@gmail.com>
2025-05-15 16:50:34 +0200

    :construction:

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
   useEffect(() => {
     console.log(router);
     console.log(router.query);
-    if (router.isReady && Object.keys(router.query).length === 0) {
+    if (router.isReady && Object.keys(router.query)[0] === 'sp') {
       return decodeURIComponent('https://d26paarabrky6y.cloudfront.net/');
     }
   }, [router]);
