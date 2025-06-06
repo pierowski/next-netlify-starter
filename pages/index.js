5785c5fc6aa1135cb28a0030bfe0afbebc5e0140
pierowski <pierpaoloroberto@gmail.com>
2025-06-06 15:32:45 +0200

    :construction:

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   const router = useRouter();
 
   const getFullUrl = (params) => {
-    const baseUrl = 'https://agricoltura-coll.regione.lazio.it/uma/home';
+    const baseUrl = 'https://d26paarabrky6y.cloudfront.net/';
     let paramsString = '?';
 
     Object.entries(params).forEach((param) => {
